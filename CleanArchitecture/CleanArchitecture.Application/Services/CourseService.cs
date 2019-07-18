5c3c15d4de676f0f2f9732937297c264df144919
Goran Trajkovic <gtrajkovic@teksavvy.ca>
2019-07-18 13:36:22 -0400

    Commit implementations of Service and Repository GetCourses Methods

---
+++ CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Interfaces;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            this.courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return context.Courses;
+        }
+    }
+}
