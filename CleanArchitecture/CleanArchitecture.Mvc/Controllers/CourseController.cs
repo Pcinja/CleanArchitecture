03acebdfb21c05bda1cfc976e78a551da4624348
Goran Trajkovic <gtrajkovic@teksavvy.ca>
2019-07-19 08:07:02 -0400

    Commit changes for presentation Course controller, views, styling, authentication

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
