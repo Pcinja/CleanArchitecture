c33187e6778d67933bafdc9eee09d4467d5f054a
Goran Trajkovic <gtrajkovic@teksavvy.ca>
2020-01-20 13:22:02 -0500

    Initial commit for CleanArchitecture MVC 3.1 Project

---
+++ CleanArchitecture/CleanArchitecture/Data/Migrations/20200120181236_Initial Migration .net core3.1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Data.Migrations
+{
+    public partial class InitialMigrationnetcore31 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
